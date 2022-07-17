f843fb73f3c0b74fbeb5626d7ee1ae4b69b0956e
Knight God <hoangphat5393@gmail.com>
2022-07-17 20:07:11 +0700

    update

---
+++ modules/icon_fixed.php
@@ -0,0 +1,5 @@
+<div class="contact-fixed">
+    <div class="zalo">
+        <a href="<?=$atz->site_url['main']?>" title="<?=SETTING['Setting_Title']?>"><img src="<?=$atz->site_url['main']?>assets/images/icon/zalo-icon.svg" alt="<?=SETTING['Setting_Title']?>" class="img-fluid"></a>
+    </div>
+</div>
